6c7631876873a0c258df052704b70ee5211c631a
AlexBor83 <rabota_for_alex@mail.ru>
2020-10-31 23:32:33 +0400

    1 задание

--- js/leson1.js
+++ js/leson1.js
@@ -1,7 +1,7 @@
 /* Задание на урок:
 
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+'Сколько фильмов вы уже посмотрели?'+ 
 
 2) Создать объект personalMovieDB и в него поместить такие свойства:
     - count - сюда передается ответ на первый вопрос
@@ -22,16 +22,30 @@
 Проверить, чтобы все работало без ошибок в консоли */
 
 "use strict";
-
-// const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
-// prompt("Сколько фильмов вы уже посмотрели?");
-// console.log('1');
-
-const answers = [];
+ 
+/* const answers = [];
 
 answers[0] = prompt('как ваше имя?', '');
 answers[1] = prompt('как ваше фамилия?', '');
-answers[2] = prompt('Сколько лет?', '');
+answers[2] = prompt('Сколько лет?', ''); */
 
-document.write (answers);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {}, 
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = prompt('На сколько оцените его?', '');
+
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = prompt('На сколько оцените его?', ' ');
+
+personalMovieDB.movies [a] =b;
+personalMovieDB.movies [c] =d;
+
+console.log(personalMovieDB);
