c5d570fb1c4cbf23d6445042e216433f0dafbc8d
AlexBor83 <rabota_for_alex@mail.ru>
2020-11-07 19:33:15 +0400

    Окончил 3 урок

--- js/1 cikl.js
+++ js/1 cikl.js
@@ -46,3 +46,11 @@ console.log(Math.round(num));
 const test = '12.2px';
 
 console.log(parseFloat(test));
+
+let a = 3
+ function addTwo(x) {
+let ret = x + 2
+  return ret
+}
+let b = addTwo(a)
+console.log(b)
